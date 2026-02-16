c3fed7bb2ec58c2d06eb486b9efc4bae200defa2
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2026-02-16 02:44:19 +0200

    remove extra annotations

--- lab1/src/main/java/mouse/univ/algorithm/hs/HSMessage.java
+++ lab1/src/main/java/mouse/univ/algorithm/hs/HSMessage.java
@@ -2,14 +2,12 @@ package mouse.univ.algorithm.hs;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NonNull;
 import mouse.univ.algorithm.Direction;
 
 @Data
 @AllArgsConstructor
 public class HSMessage {
     private Long sender;
-    @NonNull
     private Long uid;
     private Direction direction;
     private int hopCount;
--- lab1/src/main/java/mouse/univ/algorithm/hs/HSState.java
+++ lab1/src/main/java/mouse/univ/algorithm/hs/HSState.java
@@ -2,21 +2,19 @@ package mouse.univ.algorithm.hs;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NonNull;
 import mouse.univ.algorithm.Direction;
 import mouse.univ.algorithm.Status;
 
 @Data
 @AllArgsConstructor
 public class HSState {
-    @NonNull
     private final Long uid;
     private final Status status;
     private final Integer phase;
     private final HSMessage sendMinus;
     private final HSMessage sendPlus;
 
-    public HSState(@NonNull Long uid) {
+    public HSState(Long uid) {
         this.uid = uid;
         this.status = Status.UNKNOWN;
         phase = 0;
