dc00412e7fd3265fe93ac9918eb011657fbe5c25
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2026-03-30 02:18:36 +0300

    remove deprecated classes

--- lab5/src/main/java/mouse/univ/distance/Distance.java
+++
@@ -1,6 +0,0 @@
-package mouse.univ.distance;
-
-public class Distance {
-    private String vertexId;
-    private int distance;
-}
--- lab5/src/main/java/mouse/univ/distance/DistanceCalculator.java
+++
@@ -1,7 +0,0 @@
-package mouse.univ.distance;
-
-import java.util.List;
-
-public interface DistanceCalculator {
-    List<Distance> find(OrientedGraph graph, String startVertexId);
-}
